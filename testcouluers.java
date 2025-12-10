d656aadb13a01e85906ab5b568c25bb0363880cd
Siclemon <simon.jeckert@laposte.net>
2025-12-10 15:46:02 +0100

    coul

---
+++ testcouluers.java
@@ -0,0 +1,40 @@
+public class testcouluers {
+
+    public static void main(String[] args) {
+        String	SANE = "\u001B[0m";
+
+         String	HIGH_INTENSITY		= "\u001B[1m";
+         String	LOW_INTENSITY		= "\u001B[2m";
+
+        String	ITALIC				= "\u001B[3m";
+         String	UNDERLINE			= "\u001B[4m";
+        String	BLINK				= "\u001B[5m";
+        String	RAPID_BLINK			= "\u001B[6m";
+        String	REVERSE_VIDEO		= "\u001B[7m";
+        String	INVISIBLE_TEXT		= "\u001B[8m";
+        String	BLACK				= "\u001B[30m";
+        String	RED					= "\u001B[31m";
+        String	GREEN				= "\u001B[92m";
+        String	YELLOW				= "\u001B[33m";
+        String	BLUE				= "\u001B[34m";
+        String	MAGENTA				= "\u001B[35m";
+        String	CYAN				= "\u001B[36m";
+       	String WHITE				= "\u001B[37m";
+
+        String	BACKGROUND_BLACK	= "\u001B[40m";
+        String	BACKGROUND_RED		= "\u001B[41m";
+        String	BACKGROUND_GREEN	= "\u001B[42m";
+        String	BACKGROUND_YELLOW	= "\u001B[43m";
+        String	BACKGROUND_BLUE		= "\u001B[44m";
+        String	BACKGROUND_MAGENTA	= "\u001B[45m";
+        String	BACKGROUND_CYAN		= "\u001B[46m";
+       	String BACKGROUND_WHITE	    = "\u001B[47m";
+
+        System.out.println("\033[41;5;1;93m"+"caca"+"\033[0m");
+        System.out.println("hqcocql");
+
+
+
+
+    }
+}
