07116e283feb2755515a9a69266633beabb3c791
Siclemon <simon.jeckert@laposte.net>
2026-01-11 22:30:29 +0100

    rien

--- curseur.java
+++ curseur.java
@@ -1,10 +1,45 @@
+
+
 public class curseur {
     public static void main(String[] args) {
         
-        for (int i=0; i<10; i++) {
-            System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+        for (int a=0; a<256; a+=30) {
+
+            System.out.print("\033\143");
+
+            System.out.print("\033[?25l");
+
+
+            for (int b =0; b<256 ; b+=6) {
+                for (int c =0; c<256 ; c+=6 ) {
+
+                    System.out.print("\033[38;2;"+a+";"+b+";"+c+";2m██"+"\033[0m");
+                }
+                System.out.println();
+            }
+
+            try {
+                Thread.sleep(70);
+            } catch (InterruptedException ex) {
+            }
+
+        }
+        
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
         }
 
-        System.out.print("\u001B[A".repeat(3)+"\u001B[C".repeat(3)+"o");
+        
+        System.out.print("\033[s");
+        System.out.print("\033[15;30H"+"AAA");
+        System.out.print("\033[1;1H"+"bb");
+        System.out.print("\033[u");
+        System.out.print("\033[?25h");
+        
+
+        //while(true);
     }
 }
--- test.java
+++ test.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-import java.util.Scanner;
 
 
 public class test {
@@ -13,20 +12,28 @@ public class test {
         // System.out.println("\033[38;5;56mAAAAAAAAAAAAAA"+reset);
         // System.out.println("\033[38;5;57mAAAAAAAAAAAAAA"+reset);
 
-        for (int j = 0; j < 2; j++) {
-            for (int i = 0; i < 257; i++) {
-                System.out.println("\033["+(38+j*10)+";5;"+i+"mAAAAAAAAAAAAAA"+reset);
-            }
-        }
+        // for (int j = 0; j < 2; j++) {
+        //     for (int i = 0; i < 257; i++) {
+        //         System.out.println("\033["+(38+j*10)+";5;"+i+"mAAAAAAAAAAAAAA"+reset);
+        //     }
+        // }
 
-        for (int r =0; r<256 ; r+=25) {
-            for (int g =0; g<256 ; g+=25) {
-                for (int b =0; b<256 ; b+=25) {
+        for (int r =0; r<256 ; r+=5) {
+            for (int g =0; g<256 ; g+=5) {
+                for (int b =0; b<256 ; b+=5) {
                     a= rng.nextInt(256);
                     c= rng.nextInt(256);
                     d= rng.nextInt(256);
-                    System.out.print("\033[38;2;"+r+";"+g+";"+b+";4m██"+reset);
+                    System.out.print("\033[38;2;"+r+";"+g+";"+b+"m██"+reset);
                 }
+                System.out.print(" ");
+                // for (int b =0; b<256 ; b+=10) {
+                //     System.out.print("\033[38;2;"+r+";"+g+";"+b+"m██"+reset);
+                // }
+                // System.out.print(" "); //clignotant
+                // for (int b =0; b<256 ; b+=25) {
+                //     System.out.print("\033[38;2;"+r+";"+g+";"+b+";5m██"+reset);
+                // }
                 System.out.println();
             }
         }
