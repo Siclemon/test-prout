1ee95c613716adc9f817ec9acee91f293c0ce7f7
Siclemon <simon.jeckert@laposte.net>
2026-02-03 21:58:28 +0100

    color guesser ok !

--- ColorGuess.java
+++ ColorGuess.java
@@ -4,20 +4,24 @@ import java.util.Scanner;
 public class ColorGuess {
     public static void main(String[] args) {
         int[] couleur;
+        double result;
 
 
         couleur = newColor();
         display(couleur);
+        result = compareAll(input(), couleur);
+        System.out.printf("\nRésultat :   %.1f %%",result);
+        System.out.print("\nRéponse :    "+toHex(couleur)+"\n");
 
 
     }
 
     private static void display(int[] color) {
         System.out.print("\033\143");
-        for (int i = 0; i<7; i++) {
-            System.out.println("\033[48;2;"+color[0]+";"+color[1]+";"+color[2]+"m              ");
+        for (int i = 0; i<9; i++) {
+            System.out.println("\033[48;2;"+color[0]+";"+color[1]+";"+color[2]+"m                  "+"\033[m");
         }
-        System.out.print("\n\nCode hexadécimal :\n> ");
+        System.out.print("\n\n\033[4mCode hexadécimal :\033[m\n> #");
     }
 
     private static int[] newColor() {
@@ -29,14 +33,31 @@ public class ColorGuess {
         Scanner sc = new Scanner(System.in);
         String ret = null;
 
-        while (ret.length()!=6)
+        do {
             ret = sc.nextLine();
-
+            if (ret.toLowerCase().trim().equals("stop"))
+                System.exit(1);
+        } while (ret.length()!=6);
         sc.close();
         return ret;
     }
 
-    private static double compare(String input, int[] color) {
-        
+    private static double compareAll(String input, int[] color) {
+        double result = 0;
+        for (int i=0; i<3; i++)
+            result += compare(input.substring(i*2,i*2+2), color[i]);
+        result = result/3*100;
+        return result;
+    }
+
+    private static double compare(String input, int color) {
+        int inp = Integer.parseInt(input, 16);
+        return (256.0-Math.abs(color-inp))/256.0;
+    }
+
+    private static String toHex(int[] color) {
+        String ret;
+        ret = Integer.toHexString(color[0]) + Integer.toHexString(color[1]) + Integer.toHexString(color[2]);
+        return ret;
     }
 }
